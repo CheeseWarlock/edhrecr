9e392c3206ee442d738749d16971e1fe830d5dfa
Joe <joe.cardwell@gmail.com>
2025-05-16 18:12:19 -0400

    Fix build temporarily

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,52 +1,11 @@
 import postgres from 'postgres';
 // import TEST_DATA from '../lib/test.json';
 // import BULK_DATA from '../lib/oracle-cards-20250514210930.json';
-import MINI_BULK_DATA from '../lib/a-few-cards.json';
+// import MINI_BULK_DATA from '../lib/a-few-cards.json';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-interface ScryfallCard {
-  name: string;
-  image_uris?: {
-    normal?: string;
-  };
-  edhrec_rank?: number;
-}
-
-export async function createCardsTable() {
-  await sql`
-    CREATE TABLE IF NOT EXISTS cards (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      image_url VARCHAR(255) NOT NULL,
-      edhrec_rank INT NOT NULL
-    );
-    `;
-}
-
-export async function createCollectionTable() {
-  await sql`
-    CREATE TABLE IF NOT EXISTS dailycollections (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      date DATE NOT NULL,
-      description TEXT NOT NULL
-    )
-  `;
-}
-
-export async function createCollectionCardTable() {
-  await sql`
-    CREATE TABLE IF NOT EXISTS collectioncards (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      collection_id UUID NOT NULL,
-      card_id UUID NOT NULL,
-      FOREIGN KEY (collection_id) REFERENCES dailycollections(id),
-      FOREIGN KEY (card_id) REFERENCES cards(id)
-    )
-  `;
-}
-
-export async function getCardsForDate(date: string) {
+async function getCardsForDate(date: string) {
   return await sql`
     SELECT c.*
     FROM cards c
@@ -56,7 +15,7 @@ export async function getCardsForDate(date: string) {
   `;
 }
 
-export async function getDailyCards() {
+async function getDailyCards() {
   const today = (new Date()).toISOString().slice(0, 10);
 
   const precomputed = await getCardsForDate(today);
@@ -90,64 +49,6 @@ export async function getDailyCards() {
   `;
 
   return randomCards;
-}
-
-export async function populateDummyCollection() {
-  const today = (new Date()).toISOString().slice(0, 10);
-  return await sql`
-    INSERT INTO dailycollections (date, description)
-    VALUES (${today}, 'Dummy data please ignore')
-  `;
-}
-
-export async function populateDummyCollectionCards() {
-  const cards = await sql`SELECT c.id FROM cards c LIMIT 5`;
-  const collection = await sql`SELECT d.id FROM dailycollections d LIMIT 1`;
-
-  const cardIds = cards.map((card) => card.id);
-  const collectionId = collection[0].id;
-  const insertedCollectionCards = await Promise.all(
-    cardIds.map(async (cardId) => {
-      return await sql`
-        INSERT INTO collectioncards (collection_id, card_id)
-        VALUES (${collectionId}, ${cardId})
-      `;
-    })
-  );
-  return insertedCollectionCards;
-}
-
-export async function clearCardTables() {
-  const result = await sql.begin((sql) => [
-    sql`TRUNCATE TABLE cards CASCADE`,
-    sql`TRUNCATE TABLE collectioncards CASCADE`,
-    sql`TRUNCATE TABLE dailycollections CASCADE`
-  ]);
-  return result;
-}
-
-export async function populateSomeRealCards(count: number, offset: number) {
-  const cardBlock = MINI_BULK_DATA.slice(offset, offset + count).filter(card => card.image_uris?.normal && card.edhrec_rank).map((card) => ({
-    name: card.name,
-    image_url: card.image_uris.normal,
-    edhrec_rank: card.edhrec_rank
-  }));
-
-  return await sql`INSERT INTO cards ${sql(cardBlock, 'name', 'image_url', 'edhrec_rank')}`;
-}
-
-export async function populateFromBulkData(cards: ScryfallCard[], count: number, offset: number) {
-  const cardBlock = cards.slice(offset, offset + count).map((card) => ({
-    name: card.name,
-    image_url: card.image_uris!.normal,
-    edhrec_rank: card.edhrec_rank
-  }));
-
-  if (cardBlock.length === 0) {
-    return;
-  }
-
-  return await sql`INSERT INTO cards ${sql(cardBlock, 'name', 'image_url', 'edhrec_rank')}`;
 }
 
 export async function getCards() {
