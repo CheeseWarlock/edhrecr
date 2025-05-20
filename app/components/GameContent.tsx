1afa39731f6f0e40933becc09015f62a7abbff36
Joe <joe.cardwell@gmail.com>
2025-05-20 10:00:34 -0400

    Use local storage and prevent render before loading from it

--- app/components/GameContent.tsx
+++ app/components/GameContent.tsx
@@ -1,17 +1,26 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card } from '../types';
 import { GameArea } from './GameArea';
 import { TopBar } from './TopBar';
 import { InfoOverlay } from './InfoOverlay';
+import { useLocalStorage } from '../utils/useLocalStorage';
 
 interface GameContentProps {
   cards: Card[];
 }
 
 export function GameContent({ cards }: GameContentProps) {
-  const [isInfoOpen, setIsInfoOpen] = useState(false);
+  const [hasSeenInfo, setHasSeenInfo] = useLocalStorage("hasSeenInfo", false);
+  const [isInfoOpen, setIsInfoOpen] = useState(!hasSeenInfo);
+  const [hasMounted, setHasMounted] = useState(false);
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  if (!hasMounted) {
+    return null;
+  }
 
   return (
     <main className="items-center justify-items-center min-h-screen md:p-8 bg-[#222] flex justify-center pt-16 md:pt-16">
@@ -19,7 +28,10 @@ export function GameContent({ cards }: GameContentProps) {
       <div className="flex flex-col h-full row-start-2">
         <GameArea cards={cards} />
       </div>
-      <InfoOverlay isOpen={isInfoOpen} onClose={() => setIsInfoOpen(false)} />
+      <InfoOverlay isOpen={isInfoOpen} onClose={() => {
+        setIsInfoOpen(false)
+        setHasSeenInfo(true)
+      }}/>
     </main>
   );
 } 
--- app/utils/useLocalStorage.ts
+++ app/utils/useLocalStorage.ts
@@ -1,13 +1,20 @@
+'use client';
+
 import { useEffect, useState } from "react"
 
 export const useLocalStorage = (key: string, initialValue: any) => {
     const [value, setValue] = useState(() => {
-        const item = localStorage.getItem(key);
-        return item ? JSON.parse(item) : initialValue;
+        if (typeof window !== 'undefined') {
+            const item = window?.localStorage?.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        }
+        return initialValue;
     });
 
     useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(value));
+        if (typeof window !== 'undefined') {
+            window?.localStorage?.setItem(key, JSON.stringify(value));
+        }
     }, [value, key]);
 
     return [value, setValue];
