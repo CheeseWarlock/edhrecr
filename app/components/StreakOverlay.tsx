1358a4a7bf0710b4f4d6f1cf5c7c449b5ec8b544
Joe <joe.cardwell@gmail.com>
2025-05-22 10:49:07 -0400

    Build a simple chart to display guess counts

--- app/components/StreakOverlay.tsx
+++ app/components/StreakOverlay.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Metamorphous } from "next/font/google";
 import { OverlayFrame } from './OverlayFrame';
 import { StreakStatus } from '../lib/streak';
+import TinyChart from './TinyChart';
 const metamorphous = Metamorphous({ 
   weight: '400',
   subsets: ["latin"],
@@ -23,22 +24,36 @@ export function StreakOverlay({ isOpen, onClose, streak }: StreakOverlayProps) {
           <p className="text-xl mt-2">day{streak.streakLength == 1 ? "" : "s"} in a row!</p>
         </div>
         {streak.isTodayDone && streak.streakLength > 0 ? (
-          <p className="text-center mt-8">
+          <p className="text-center my-2 mb-10">
             Keep coming back daily to maintain your streak!
           </p>
         ) : streak.isTodayDone ? (
-          <p className="text-center mt-8">
+          <p className="text-center my-2 mb-10">
             Come back tomorrow to restart your streak!
           </p>
         ) : streak.isStreakActive ? (
-          <p className="text-center mt-8">
+          <p className="text-center my-2 mb-10">
             Finish today&apos;s challenge to keep your streak alive!
           </p>
         ) : (
-          <p className="text-center mt-8">
+          <p className="text-center my-2 mb-10">
             Finish today&apos;s challenge to start your streak!
           </p>
         )}
+        <h2 className={`text-2xl md:text-4xl font-bold mb-2 ${metamorphous.className}`}>Your Stats</h2>
+        <div className="md:px-6 lg:px-12">
+          <TinyChart data={[ { label: '1', value: 2 }, { label: '2', value: 4 }, { label: '3', value: 10 }, { label: '4', value: 3 }, { label: '5', value: 2 }, { label: 'X', value: 1 }]} />
+        </div>
+        <div className="flex flex-row text-lg justify-around">
+          <div>
+          <span>Played:&nbsp;</span>
+          <span>112</span>
+          </div>
+          <div>
+            <span>Avg:&nbsp;</span>
+            <span>2.38</span>
+          </div>
+        </div>
       </div>
     </OverlayFrame>
   );
---
+++ app/components/TinyChart.tsx
@@ -0,0 +1,38 @@
+const COLOR_MAP: Record<string, string> = {
+    '1': '#2694AF',
+    '2': '#7C9B13',
+    '3': '#7C9B13',
+    '4': '#7C9B13',
+    '5': '#7C9B13',
+    'X': '#DC5E25'
+}
+
+export default function TinyChart({ data } : { data: { label: string, value: number }[] }) {
+    const max = data.reduce((a, v) => Math.max(a, v.value), 0);
+    const sum = data.reduce((a, v) => a + v.value, 0);
+    return (
+        <div className="flex flex-col h-[200px]">
+            <div className="flex flex-row items-end flex-grow-1">
+                {data.map((datum, idx) => {
+                    return <div
+                    key={idx}
+                    style={{
+                        background: `${COLOR_MAP[datum.label]}`,
+                        height: `calc(100% * ${datum.value} / ${max})`,
+                        width: `calc(100% / ${data.length})`}}
+                        className={`p-1 text-center text-black md:mx-4 rounded-md`}>
+                        <span
+                            className={` ${datum.value <= (sum / 20) ? `rounded-sm p-1 relative top-[-36px]` : '' }`}
+                            style={{ background: `${COLOR_MAP[datum.label]}` }}
+                            >×{datum.value}</span>
+                </div>
+                })}
+            </div>
+            <div className="flex flex-row">
+                {data.map((datum, idx) => {
+                    return <div key={idx} className="text-center" style={{ height: `30px`, width: `calc(100% / ${data.length})`}}>{datum.label}</div>
+                })}
+            </div>
+        </div>
+    );
+}
