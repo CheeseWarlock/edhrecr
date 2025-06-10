1b3589b0756bfdac29735b3ac86965e0bc391298
Joe <joe.cardwell@gmail.com>
2025-06-10 00:01:01 -0400

    Add a sharing link

--- app/components/FailurePanel.tsx
+++ app/components/FailurePanel.tsx
@@ -1,9 +1,13 @@
 import { Card } from "../types"
 import { CardImage } from "./CardImage"
+import ShareLink from "./ShareLink"
 
-export default function FailurePanel({ cards, isPastGame }: {
+export default function FailurePanel({ cards, isPastGame, date, guesses, solution }: {
     cards: Card[],
-    isPastGame: boolean
+    isPastGame: boolean,
+    date: string | undefined,
+    guesses: Card[][],
+    solution: Card[]
 }) {
     return <>
         <div className="w-full flex flex-row" style={{
@@ -24,5 +28,6 @@ export default function FailurePanel({ cards, isPastGame }: {
             <span className="text-white text-2xl font-bold bg-[rgba(68,68,68,0.7)] p-2 rounded-t-md">No more guesses!</span>
             {!isPastGame && <span className="text-white md:text-lg text-center bg-[rgba(68,68,68,0.7)] p-2 rounded-md">Come back tomorrow for another challenge.</span>}
             <span className="text-white md:text-lg text-center bg-[rgba(68,68,68,0.7)] p-2 rounded-b-md">The correct order is shown here.</span>
+            <span className="pointer-events-auto"><ShareLink guesses={guesses} solution={solution} today={date} win={false} /></span>
         </div></>
 }
---
+++ app/components/ShareLink.tsx
@@ -0,0 +1,41 @@
+import { FaShareNodes } from "react-icons/fa6";
+import { Card } from "../types";
+import { useState } from "react";
+
+function ShareLink({ today, guesses, solution, win }: { today: string | undefined, guesses: Card[][], solution: Card[], win: boolean }) {
+  const icon = win ? '✓' :  '✗';
+  const share = (today: string | undefined, guesses: Card[][], solution: Card[]) => {
+    const resultString = guesses.map((guess) => {
+      return guess.map((card, index) => {
+        return (index === solution.indexOf(card) ? "🟩" : "⬛")
+      }).join("");
+    }).join('\n');
+    const shareData = {
+      text: `EDHRanker for ${today} ${icon}\n${resultString}`,
+      url: "https://edhranker.vercel.app",
+      title: "EDHRanker",
+    }
+
+    const useNavigatorShare = /android|iphone|ipad|ipod|webos/i.test(navigator.userAgent) &&
+      !/firefox/i.test(navigator.userAgent) && navigator.canShare && navigator.canShare(shareData);
+    if (!useNavigatorShare) {
+      navigator.clipboard.writeText(`${shareData.text}\n\n${shareData.url}`);
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 2000);
+    } else {
+      navigator.share(shareData);
+    }
+  }
+
+  const [isCopied, setIsCopied] = useState(false);
+  return (
+    <div className="flex items-center gap-2 cursor-pointer" onClick={() => {
+      share(today, guesses, solution);
+    }}>
+      <FaShareNodes/>
+      <span>{isCopied ? "Copied!" : "Share"}</span>
+    </div>
+  );
+}
+
+export default ShareLink;
