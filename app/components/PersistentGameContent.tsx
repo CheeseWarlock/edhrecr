7bd858a76d1ea6106db2827efb4b69be5d79fac8
Joe <joe.cardwell@gmail.com>
2025-06-16 11:44:21 -0400

    Ensure local storage is never called from server

--- app/components/PersistentGameContent.tsx
+++ app/components/PersistentGameContent.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { Card } from '../types';
 import { GameContent } from './GameContent';
 import { getUserGuesses, setUserGuesses } from '../utils/localStorageUtils';
@@ -12,7 +12,13 @@ interface PersistentGameContentProps {
 }
 
 export function PersistentGameContent({ cards, date, today }: PersistentGameContentProps) {
-  const [storedGuesses, setStoredGuesses] = useState<number[][]>(getUserGuesses(date));
+  const [storedGuesses, setStoredGuesses] = useState<number[][]>([]);
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setStoredGuesses(getUserGuesses(date));
+    setHasMounted(true);
+  }, [date]);
 
   const persistGuesses = useCallback((guesses: Card[][]) => {
     const cardsWithTodaysDate = guesses.map(guess => guess.map(card => cards.findIndex(c => c.id === card.id)));
@@ -21,6 +27,10 @@ export function PersistentGameContent({ cards, date, today }: PersistentGameCont
   }, [cards, date]);
 
   const storedGuessesAsCards = useMemo(() => storedGuesses.map(guess => guess.map(cardIdx => cards[cardIdx])), [storedGuesses, cards]);
+  
+  if (!hasMounted) {
+    return null;
+  }
 
   return (
     <GameContent 
