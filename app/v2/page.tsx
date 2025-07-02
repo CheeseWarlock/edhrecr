bec96bc9212058c3dd38851ed15337372471ed55
Joe <joe.cardwell@gmail.com>
2025-07-02 11:27:49 -0400

    Add notice for v2 game

--- app/v2/page.tsx
+++ app/v2/page.tsx
@@ -2,7 +2,11 @@ import { getCardsForToday_better } from "../lib/daily-cards";
 import { NonPersistentGameContent } from "../components/NonPersistentGameContent";
 
 export default async function Home() {
+  console.log("v2 page");
   const dailyCardsData = await getCardsForToday_better();
   console.log(dailyCardsData);
-  return <NonPersistentGameContent cards={dailyCardsData.collection.cards} date={dailyCardsData.collection.date} today={dailyCardsData.today} />;
+  return <>
+    <span className="text-white text-center absolute top-40 left-0 w-full z-50 text-2xl">v2 Content Preview, will not count towards streak</span>
+    <NonPersistentGameContent cards={dailyCardsData.collection.cards} date={dailyCardsData.collection.date} today={dailyCardsData.today} />
+  </>;
 }
