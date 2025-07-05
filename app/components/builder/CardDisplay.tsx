4537b2438da11bbabbeec681bb1017efceb334c1
Joe <joe.cardwell@gmail.com>
2025-07-05 10:30:15 -0400

    Split out removeable card

--- app/components/builder/CardDisplay.tsx
+++ app/components/builder/CardDisplay.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Card } from "@/app/types";
+import RemovableCardWithData from "./RemovableCardWithData";
 
 interface CardDisplayProps {
   selectedCards: Card[];
@@ -20,33 +21,11 @@ export default function CardDisplay({ selectedCards, onRemoveCard }: CardDisplay
         </div>
       )}
       {selectedCards.map((card) => (
-        <div className="flex flex-col items-center gap-2" key={card.id}>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src={card.image_url}
-            alt={card.name}
-            width={256}
-            height={357} 
-            className={`select-none object-contain mw-[256px] mh-[357px] rounded-[5%]`} 
-          />
-          <div className="flex flex-row items-center justify-center">
-            <span className="m-2 text-white">#{card.edhrec_rank}</span>
-            <button 
-              onClick={() => window.open(`https://edhrec.com/route/?cc=${card.name}`, '_blank')} 
-              className="bg-mana-blue text-white px-2 py-1 rounded-md cursor-pointer"
-            >
-              EDHRec
-            </button>
-          </div>
-          <div className="flex flex-row justify-center">
-            <button 
-              onClick={() => onRemoveCard(card.id)} 
-              className="bg-mana-red text-white px-2 py-1 rounded-md cursor-pointer"
-            >
-              Remove
-            </button>
-          </div>
-        </div>
+        <RemovableCardWithData
+          key={card.id}
+          card={card}
+          onRemoveCard={onRemoveCard}
+        />
       ))}
     </div>
   );
